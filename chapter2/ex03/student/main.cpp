81965cf08f32eb17f693d494aebe314b3b54a637
Lovinson Dieujuste <101741874+Wisesofthemall@users.noreply.github.com>
2025-01-23 18:51:14 +0000

    Testing Thu Jan 23 2025 18:51:14 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex03/student/main.cpp
+++ chapter2/ex03/student/main.cpp
@@ -1,16 +1,19 @@
-//include statement(s)
-
-//using namespace statement
-
-
+#include <iostream>
+using namespace std;
 int main()
 {
+    int num1, num2, num3, average;
+    num1 = 125;
+    num2 = 28;
+    num3 = -25;
+    average = 0;
+    average = (num1 + num2 + num3) / 3;
 
-//variable declaration
+    cout << "The average is: " << average << endl;
 
 
-//executable statements
 
 
-//return 0;
+return 0;
 }
+
