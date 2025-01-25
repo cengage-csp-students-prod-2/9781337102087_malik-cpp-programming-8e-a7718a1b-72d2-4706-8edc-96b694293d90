16573e6ecbb6154cc4c8f8514fe4a1146506f387
Lovinson Dieujuste <101741874+Wisesofthemall@users.noreply.github.com>
2025-01-25 21:10:37 +0000

    Testing Sat Jan 25 2025 21:10:37 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex06/student/main.cpp
+++ chapter2/ex06/student/main.cpp
@@ -1,15 +1,20 @@
 //include statement(s)
-
+#include <iostream>
 //using namespace statement
-
+using namespace std;
 
 int main()
 {
 //variable declaration
+  string hello;
+  cin >> hello;
+  cout << hello << endl;
+
 
 
 //executable statements
 
 
 //return statement
+return 0;
 }
