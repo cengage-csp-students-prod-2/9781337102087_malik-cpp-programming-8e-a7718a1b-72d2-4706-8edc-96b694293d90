48c7d041d2e1d4a7d0ac26035d2386b01336f092
Lovinson Dieujuste <101741874+Wisesofthemall@users.noreply.github.com>
2025-02-06 23:51:16 +0000

    Testing Thu Feb 06 2025 23:51:16 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex29/student/main.cpp
+++ chapter5/ex29/student/main.cpp
@@ -2,7 +2,28 @@
 
 using namespace std;
 
+// Function to compute factorial
+long long factorial(int num) {
+    long long fact = 1;
+    for (int i = 1; i <= num; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
 int main() {
-    // Write your main here
+    int input;
+
+    // Prompt the user for input
+    cout << "Enter a nonnegative integer: ";
+    cin >> input;
+
+    // Validate input
+    if (input < 0) {
+        cout << "Factorial is not defined for negative numbers." << endl;
+    } else {
+        cout << input << "! = " << factorial(input) << endl;
+    }
+
     return 0;
 }
