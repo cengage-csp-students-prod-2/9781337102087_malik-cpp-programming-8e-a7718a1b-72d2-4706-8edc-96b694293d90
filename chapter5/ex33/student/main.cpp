98137b7c8edd65c1a964a9ffc9853ccba10dcf84
Lovinson Dieujuste <101741874+Wisesofthemall@users.noreply.github.com>
2025-02-07 00:19:39 +0000

    Testing Fri Feb 07 2025 00:19:39 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex33/student/main.cpp
+++ chapter5/ex33/student/main.cpp
@@ -3,6 +3,28 @@
 using namespace std;
 
 int main() {
-    // Write your main here
+    int a, b, t;
+    
+    // Prompt the user for input
+    cout << "Enter the time for the first dish (a): ";
+    cin >> a;
+    cout << "Enter the additional time per dish (b): ";
+    cin >> b;
+    cout << "Enter the total available time (t): ";
+    cin >> t;
+
+    int dishes = 0;
+    int timeSpent = 0;
+    
+    // Calculate the number of dishes Bianca can prepare
+    while (timeSpent + a <= t) {
+        timeSpent += a;
+        dishes++;
+        a += b;  // Increase time for the next dish
+    }
+
+    // Output the number of dishes prepared
+    cout << "Number of dishes Bianca can prepare: " << dishes << endl;
+
     return 0;
 }
