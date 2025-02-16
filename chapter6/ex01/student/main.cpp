f473e9ae16e1e743511a96506b4d4b6e32e5e195
Lovinson Dieujuste <101741874+Wisesofthemall@users.noreply.github.com>
2025-02-16 01:54:29 +0000

    Testing Sun Feb 16 2025 01:54:29 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex01/student/main.cpp
+++ chapter6/ex01/student/main.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
+#include <algorithm>  // For transform()
 
 using namespace std;
 
+// Function to check if a string is a palindrome (case-insensitive)
+bool isPalindrome(string str) {
+    int length = str.length();
+    
+    // Convert the string to lowercase for case insensitivity
+    transform(str.begin(), str.end(), str.begin(), ::tolower);
+
+    for (int i = 0; i < length / 2; i++) {
+        if (str[i] != str[length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main() {
-    // Write your main here
+    string input;
+    
+    cout << "Enter a string: ";
+    cin >> input;
+
+    if (isPalindrome(input)) {
+        cout << input << " is a palindrome" << endl;
+    } else {
+        cout << input << " is not a palindrome" << endl;
+    }
+
     return 0;
 }
