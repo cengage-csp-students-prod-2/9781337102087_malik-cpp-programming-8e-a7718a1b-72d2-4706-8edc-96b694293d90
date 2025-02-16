d64a3b69f67e5c2d535ab7df7766d672b5412c1f
Lovinson Dieujuste <101741874+Wisesofthemall@users.noreply.github.com>
2025-02-16 01:46:25 +0000

    Testing Sun Feb 16 2025 01:46:25 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex09/student/main.cpp
+++ chapter6/ex09/student/main.cpp
@@ -2,7 +2,26 @@
 
 using namespace std;
 
+int reverseDigit(int num) {
+    bool isNegative = num < 0;  
+    num = abs(num); 
+
+    int reversed = 0;
+    while (num > 0) {
+        reversed = reversed * 10 + (num % 10);  
+        num /= 10;  
+    }
+
+    return isNegative ? -reversed : reversed; 
+}
+
 int main() {
-    // Write your main here
+
+    int input;
+    cin >> input;
+
+    cout << "Reverse of 12345: " << reverseDigit(input) << endl;
+
+
     return 0;
 }
