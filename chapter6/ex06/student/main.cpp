f80abf39fb7fb8a5b68d791552f19b52901ac2ff
Lovinson Dieujuste <101741874+Wisesofthemall@users.noreply.github.com>
2025-02-16 02:30:07 +0000

    Testing Sun Feb 16 2025 02:30:07 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex06/student/main.cpp
+++ chapter6/ex06/student/main.cpp
@@ -2,7 +2,43 @@
 
 using namespace std;
 
+// Function for Exercise 23
+void processNumber(int num) {
+    if (num % 2 == 0) {
+        cout << "Result: " << num * 2 << endl;
+    } else {
+        cout << "Result: " << num * 5 << endl;
+    }
+}
+
+// Function for Exercise 26
+void sumAndAverage(int n, int m) {
+    if (n > m) {
+        swap(n, m);  // Ensure n is smaller for correct range
+    }
+
+    int sum = 0, count = 0;
+    for (int i = n; i <= m; i++) {
+        sum += i;
+        count++;
+    }
+
+    double average = static_cast<double>(sum) / count;
+    cout << "Sum: " << sum << " Average: " << average << endl;
+}
+
 int main() {
-    // Write your main here
+    int num, n, m;
+
+    // Exercise 23: Process a single number
+    cout << "Enter an integer: ";
+    cin >> num;
+    processNumber(num);
+
+    // Exercise 26: Process range of numbers
+    cout << "Enter two integers (separated by space): ";
+    cin >> n >> m;
+    sumAndAverage(n, m);
+
     return 0;
 }
