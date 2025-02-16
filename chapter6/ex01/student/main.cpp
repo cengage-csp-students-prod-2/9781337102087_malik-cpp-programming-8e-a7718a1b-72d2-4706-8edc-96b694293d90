cd4744522fc129e3c4ac84c37dd05ec596e19583
Lovinson Dieujuste <101741874+Wisesofthemall@users.noreply.github.com>
2025-02-16 17:17:50 +0000

    Testing Sun Feb 16 2025 17:17:50 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex01/student/main.cpp
+++ chapter6/ex01/student/main.cpp
@@ -9,19 +9,19 @@
 // isPalindrome("trYmeuemyRT")
 using namespace std;
 
-// Function to check if a string is a palindrome (case-insensitive)
+
 bool isPalindrome(string str) {
-    // Convert the string to lowercase
+
     transform(str.begin(), str.end(), str.begin(), ::tolower);
     
     int length = str.length(); 
     for (int i = 0; i < length / 2; i++) {
         if (str[i] != str[length - 1 - i]) {
             return false;
-        } // if    
-    } // for loop
+        }   
+    } 
     return true;
-}// isPalindrome
+}
 
 int main() {
     // Test strings
