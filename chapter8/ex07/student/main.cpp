915303c3687c74d03120591abe6513c9b3a209b5
Lovinson Dieujuste <101741874+Wisesofthemall@users.noreply.github.com>
2025-02-27 20:53:52 +0000

    Testing Thu Feb 27 2025 20:53:52 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex07/student/main.cpp
+++ chapter8/ex07/student/main.cpp
@@ -4,17 +4,8 @@
 
 using namespace std;
 
-const int NUM_CANDIDATES = 5;
 
-int calTotalVotes(int votes[], int size) {
-    int total = 0;
-    for (int i = 0; i < size; i++) {
-        total += votes[i];
-    }
-    return total;
-}
-
-string determineWinner(string candidates[], int votes[], int size) {
+string findWinner(string candidates[], int votes[], int size) {
     int maxVotes = votes[0];
     string winner = candidates[0];
     for (int i = 1; i < size; i++) {
@@ -26,24 +17,32 @@ string determineWinner(string candidates[], int votes[], int size) {
     return winner;
 }
 
+int calTotalVotes(int votes[], int size) {
+    int total = 0;
+    for (int i = 0; i < size; i++) {
+        total += votes[i];
+    }
+    return total;
+}
+
 int main() {
-    string candidates[NUM_CANDIDATES];
-    int votes[NUM_CANDIDATES];
+    string candidates[5];
+    int votes[5];
     
-    cout << "Enter the last names of " << NUM_CANDIDATES << " candidates and their votes received:\n";
-    for (int i = 0; i < NUM_CANDIDATES; i++) {
+    cout << "Enter the last names of " << 5 << " candidates and their votes received:\n";
+    for (int i = 0; i < 5; i++) {
         cout << "Candidate " << (i + 1) << " name: ";
         cin >> candidates[i];
         cout << "Votes received: ";
         cin >> votes[i];
     }
     
-    int totalVotes = calTotalVotes(votes, NUM_CANDIDATES);
-    string winner = determineWinner(candidates, votes, NUM_CANDIDATES);
+    int totalVotes = calTotalVotes(votes, 5);
+    string winner = findWinner(candidates, votes, 5);
     
     cout << "Candidate    Votes Received   % of Total Votes";
     cout << fixed << setprecision(2);
-    for (int i = 0; i < NUM_CANDIDATES; i++) {
+    for (int i = 0; i < 5; i++) {
         double percentage = (static_cast<double>(votes[i]) / totalVotes) * 100;
         cout << candidates[i] << "     " << votes[i] << "     " << percentage << "\n";
     }
