55db8c76abb23d714aff834782051f5de1d9bcb6
Lovinson Dieujuste <101741874+Wisesofthemall@users.noreply.github.com>
2025-04-15 19:41:32 +0000

    Testing Tue Apr 15 2025 19:41:32 GMT+0000 (Coordinated Universal Time)

--- chapter13/ex10/student/main.cpp
+++ chapter13/ex10/student/main.cpp
@@ -8,85 +8,98 @@ private:
     int denominator;
 
 public:
-    // Default constructor
-    fractionType(int num = 0, int denom = 1) {
+    // Constructor
+    fractionType() {
+        numerator = 0;
+        denominator = 1;
+    }
+
+    // Set values
+    void setFraction(int num, int denom) {
         numerator = num;
-        denominator = (denom == 0) ? 1 : denom; // prevent division by zero
+        if (denom == 0) {
+            cout << "Denominator cannot be 0. Defaulting to 1." << endl;
+            denominator = 1;
+        } else {
+            denominator = denom;
+        }
     }
 
-    // Arithmetic operator overloads
-    fractionType operator+(const fractionType& other) const {
-        int num = numerator * other.denominator + other.numerator * denominator;
-        int denom = denominator * other.denominator;
-        return fractionType(num, denom);
+    // Overload arithmetic operators
+    fractionType operator+(fractionType other) {
+        fractionType result;
+        result.numerator = numerator * other.denominator + other.numerator * denominator;
+        result.denominator = denominator * other.denominator;
+        return result;
     }
 
-    fractionType operator-(const fractionType& other) const {
-        int num = numerator * other.denominator - other.numerator * denominator;
-        int denom = denominator * other.denominator;
-        return fractionType(num, denom);
+    fractionType operator-(fractionType other) {
+        fractionType result;
+        result.numerator = numerator * other.denominator - other.numerator * denominator;
+        result.denominator = denominator * other.denominator;
+        return result;
     }
 
-    fractionType operator*(const fractionType& other) const {
-        int num = numerator * other.numerator;
-        int denom = denominator * other.denominator;
-        return fractionType(num, denom);
+    fractionType operator*(fractionType other) {
+        fractionType result;
+        result.numerator = numerator * other.numerator;
+        result.denominator = denominator * other.denominator;
+        return result;
     }
 
-    fractionType operator/(const fractionType& other) const {
-        int num = numerator * other.denominator;
-        int denom = denominator * other.numerator;
-        return fractionType(num, denom);
+    fractionType operator/(fractionType other) {
+        fractionType result;
+        result.numerator = numerator * other.denominator;
+        result.denominator = denominator * other.numerator;
+        return result;
     }
 
-    // Relational operator overloads
-    bool operator==(const fractionType& other) const {
+    // Overload comparison operators
+    bool operator==(fractionType other) {
         return numerator * other.denominator == other.numerator * denominator;
     }
 
-    bool operator!=(const fractionType& other) const {
+    bool operator!=(fractionType other) {
         return !(*this == other);
     }
 
-    bool operator<(const fractionType& other) const {
+    bool operator<(fractionType other) {
         return numerator * other.denominator < other.numerator * denominator;
     }
 
-    bool operator<=(const fractionType& other) const {
+    bool operator<=(fractionType other) {
         return *this < other || *this == other;
     }
 
-    bool operator>(const fractionType& other) const {
+    bool operator>(fractionType other) {
         return !(*this <= other);
     }
 
-    bool operator>=(const fractionType& other) const {
+    bool operator>=(fractionType other) {
         return !(*this < other);
     }
 
-    // Stream insertion and extraction
-    friend ostream& operator<<(ostream& os, const fractionType& frac) {
-        os << frac.numerator << "/" << frac.denominator;
-        return os;
+    // Overload stream operators
+    friend istream& operator>>(istream& in, fractionType& f) {
+        char slash;
+        int num, denom;
+        in >> num >> slash >> denom;
+        f.setFraction(num, denom);
+        return in;
     }
 
-    friend istream& operator>>(istream& is, fractionType& frac) {
-        char slash;
-        is >> frac.numerator >> slash >> frac.denominator;
-        if (frac.denominator == 0) {
-            cout << "Denominator cannot be zero. Setting to 1 by default.\n";
-            frac.denominator = 1;
-        }
-        return is;
+    friend ostream& operator<<(ostream& out, const fractionType& f) {
+        out << f.numerator << "/" << f.denominator;
+        return out;
     }
 };
 
-// Main function to test the class
 int main() {
     fractionType x, y, z;
 
     cout << "Enter first fraction (e.g., 2/3): ";
     cin >> x;
+
     cout << "Enter second fraction (e.g., 3/4): ";
     cin >> y;
 
@@ -102,13 +115,13 @@ int main() {
     z = x / y;
     cout << x << " / " << y << " = " << z << endl;
 
-    cout << "\nRelational Comparisons:\n";
-    cout << x << " == " << y << " : " << (x == y ? "true" : "false") << endl;
-    cout << x << " != " << y << " : " << (x != y ? "true" : "false") << endl;
-    cout << x << " < " << y << "  : " << (x < y ? "true" : "false") << endl;
-    cout << x << " <= " << y << " : " << (x <= y ? "true" : "false") << endl;
-    cout << x << " > " << y << "  : " << (x > y ? "true" : "false") << endl;
-    cout << x << " >= " << y << " : " << (x >= y ? "true" : "false") << endl;
+    cout << "\nComparison Results:\n";
+    cout << x << " == " << y << ": " << (x == y ? "true" : "false") << endl;
+    cout << x << " != " << y << ": " << (x != y ? "true" : "false") << endl;
+    cout << x << " <  " << y << ": " << (x < y ? "true" : "false") << endl;
+    cout << x << " <= " << y << ": " << (x <= y ? "true" : "false") << endl;
+    cout << x << " >  " << y << ": " << (x > y ? "true" : "false") << endl;
+    cout << x << " >= " << y << ": " << (x >= y ? "true" : "false") << endl;
 
     return 0;
 }
